66f165ca690de5a66d9868b4245a95b6f13f7bee
Lian Hirsh <lianhirsh123@gmail.com>
2020-03-25 16:06:15 +0200

    task members cmp

---
+++ frontend/src/components/member-picker.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="member-picker">
+      {{members}}
+    <header>
+      <h2 class="member-title">Board members</h2>
+    </header>
+    <h3 class="member-title">Members</h3>
+    <ul>
+      <li v-for="member in members" :key="member._id">
+          <div>{{member.username}}</div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "member-picker",
+  props: {
+    members: Array
+  }
+};
+</script>
