c75d1a98eba1841a5124574e8005f423354bf7a6
Lian Hirsh <lianhirsh123@gmail.com>
2020-03-27 15:49:00 +0300

    task-css

--- frontend/src/views/task-details.vue
+++ frontend/src/views/task-details.vue
@@ -12,38 +12,56 @@
       </div>
       <div class="details-container">
         <section class="task-data">
-          <div>
-            <h2 v-if="!isOpenName" @click.stop="toggle('isOpenName')">{{task.name}}</h2>
-            <input v-else type="text" v-model="task.name" @blur="saveName" @keyup.enter="saveName" />
+          <div class="task-name">
+            <i class="el-icon-s-claim icon"></i>
+            <div>
+              <h2 v-if="!isOpenName" @click.stop="toggle('isOpenName')">{{task.name}}</h2>
+              <input
+                v-else
+                type="text"
+                v-model="task.name"
+                @blur="saveName"
+                @keyup.enter="saveName"
+              />
+            </div>
           </div>
           <div class="list-name">In list {{list.name}}</div>
           <div class="main-data">
-            <h4>Labels</h4>
-            <div v-if="task.labels.length > 0" class="labels">
-              <span
-                v-for="label in task.labels"
-                :key="label.id"
-                :style="{backgroundColor:label.color}"
-                class="label"
-                @click="toggle('isLabelsSelected')"
-              >
-                <span class="label-title" v-if="label.title">{{label.title}}</span>
-              </span>
-            </div>
-            <div class="members">
-              <h4>Members</h4>
-              <div
-                v-if="task.members !== undefined && task.members.length > 0"
-                @click="toggle('isAddMember')"
-              >
-                <div v-for="member in task.members" :key="member._id">
-                  <img v-if="member.imgUrl" :src="member.imgUrl" class="member-img" />
-                  <avatar v-else :username="member.username" class="member"></avatar>
+            <section class="member-labels">
+              <div>
+                <div class="members">
+                  <h4 v-if="task.members.length > 0" class="sub-titles">Members</h4>
+                  <div
+                    v-if="task.members !== undefined && task.members.length > 0"
+                    @click="toggle('isAddMember')"
+                  >
+                    <div v-for="member in task.members" :key="member._id">
+                      <img v-if="member.imgUrl" :src="member.imgUrl" class="member-img" />
+                      <avatar v-else :username="member.username" class="member"></avatar>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+              <div>
+                <h4 v-if="task.labels.length > 0" class="sub-titles">Labels</h4>
+                <div v-if="task.labels.length > 0" class="labels">
+                  <span
+                    v-for="label in task.labels"
+                    :key="label.id"
+                    :style="{backgroundColor:label.color}"
+                    class="label"
+                    @click="toggle('isLabelsSelected')"
+                  >
+                    <span class="label-title" v-if="label.title">{{label.title}}</span>
+                  </span>
+                </div>
+              </div>
+            </section>
             <div class="description-content">
-              <h4>Description</h4>
+              <div class="description-title">
+                <i class="el-icon-s-unfold icon"></i>
+                <h4>Description</h4>
+              </div>
               <div
                 v-if="!isOpenDescription"
                 class="edit-area description-txt"
