9b3c841d1ad48121b2fd1d4d91bf13815bf14618
Ella <ella.brazlev@gmail.com>
2020-03-27 15:34:31 +0300

    task main btns

--- frontend/src/views/task-details.vue
+++ frontend/src/views/task-details.vue
@@ -76,6 +76,7 @@
             <activitylog v-if="isOpenActivitylog" :activitieslog="activitieslog"></activitylog>
           </div>
         </section>
+
         <section class="task-buttons">
           <h4>Add to task</h4>
           <div class="main-buttons">
