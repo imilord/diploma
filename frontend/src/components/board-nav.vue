2adc6f64d53db4035eb33dd5e5f2fb11a6800e79
air <asilkhan-al@mail.ru>
2020-05-29 01:28:03 +0600

    Search for task lists added

--- frontend/src/components/board-nav.vue
+++ frontend/src/components/board-nav.vue
@@ -1,71 +1,79 @@
 <template>
-  <section class="board-nav container">
-    <ul>
-      <li
-        class="board-name"
-        v-if="!titleClicked"
-        @click.stop="titleClicked = !titleClicked"
-      >{{boardName}}</li>
-      <li class="board-name" v-if="titleClicked">
-        <input type="text" v-model="newBoardName" @blur="changeName" @keyup.enter="changeName" />
-      </li>
-      <li v-if="members && !isMobile" class="members">
-        <div v-for="member in members" :key="member._id">
-          <img
-            v-if="member.imgUrl"
-            :src="member.imgUrl"
-            class="member-img"
-            :title="member.username"
-          />
-          <avatar v-else :username="member.username" class="member" :title="member.username"></avatar>
-        </div>
-        <span class="add-member-btn" @click="$emit('add-member')" title="Add member">
-          <font-awesome-icon class="new-add-btn" icon="plus" />
+    <section class="board-nav container">
+        <ul>
+            <li
+                    class="board-name"
+                    v-if="!titleClicked"
+                    @click.stop="titleClicked = !titleClicked"
+            >{{boardName}}
+            </li>
+            <li class="board-name" v-if="titleClicked">
+                <input type="text" v-model="newBoardName" @blur="changeName" @keyup.enter="changeName"/>
+            </li>
+            <li v-if="members && !isMobile" class="members">
+                <div v-for="member in members" :key="member._id">
+                    <img
+                            v-if="member.imgUrl"
+                            :src="member.imgUrl"
+                            class="member-img"
+                            :title="member.username"
+                    />
+                    <avatar v-else :username="member.username" class="member" :title="member.username"></avatar>
+                </div>
+                <span class="add-member-btn" @click="$emit('add-member')" title="Add member">
+          <font-awesome-icon class="new-add-btn" icon="plus"/>
         </span>
-      </li>
-    </ul>
-    <div>
-      <button @click="$emit('toggle-settings')">Menu</button>
-      <button @click="$emit('toggle-activitylog')">Activity Log</button>
-    </div>
-  </section>
+            </li>
+            <li class="board-name">
+                <section class="search">
+                    <i class="el-icon-search"></i>
+                    <input type="text" placeholder="Search task list" @input="$emit('username-changed', $event.target.value)"/>
+                </section>
+            </li>
+        </ul>
+        <div>
+            <button @click="$emit('toggle-settings')">Menu</button>
+            <button @click="$emit('toggle-activitylog')">Activity Log</button>
+        </div>
+    </section>
 </template>
 
 <script>
-import avatar from "vue-avatar";
-export default {
-  name: "board-nav",
-  props: {
-    boardName: String,
-    members: Array,
-    dueDate: Number,
-    creator: Object
-  },
-  data() {
-    return {
-      titleClicked: false,
-      newBoardName: "",
-      isMobile: false
+    import avatar from "vue-avatar";
+
+    export default {
+        name: "board-nav",
+        props: {
+            boardName: String,
+            members: Array,
+            dueDate: Number,
+            creator: Object,
+        },
+        data() {
+            return {
+                titleClicked: false,
+                newBoardName: "",
+                isMobile: false
+            };
+        },
+        methods: {
+            changeName() {
+                this.titleClicked = false;
+                this.$emit(
+                    "update-boardname",
+                    JSON.parse(JSON.stringify(this.newBoardName))
+                );
+            },
+            isMobileDevice() {
+                return window.innerWidth < 550;
+            }
+        },
+        created() {
+            this.newBoardName = this.boardName;
+            this.isMobile = this.isMobileDevice();
+        },
+        components: {
+            avatar
+        }
     };
-  },
-  methods: {
-    changeName() {
-      this.titleClicked = false;
-      this.$emit(
-        "update-boardname",
-        JSON.parse(JSON.stringify(this.newBoardName))
-      );
-    },
-    isMobileDevice() {
-      return window.innerWidth < 550;
-    }
-  },
-  created() {
-    this.newBoardName = this.boardName;
-    this.isMobile = this.isMobileDevice();
-  },
-  components: {
-    avatar
-  }
-};
 </script>
--- frontend/src/views/board-details.vue
+++ frontend/src/views/board-details.vue
@@ -1,209 +1,222 @@
 <template>
-  <section class="board-details" v-if="board" :style="{background: boardStyle}">
-    <div class="push"></div>
-    <board-nav
-      :boardName="board.name"
-      :members="board.members"
-      :dueDate="board.dueDate"
-      :creator="board.creator"
-      @toggle-activitylog="toggleActivitylog"
-      @toggle-settings="toggleSettings"
-      @update-boardname="updateBoardname"
-      @add-member="addMember"
-    ></board-nav>
-    <add-member
-      v-if="isAddMember"
-      :users="users"
-      :members="board.members"
-      @add-new-member="addNewMember"
-      @remove-member="removeMember"
-      @close-add-member="isAddMember=false"
-    ></add-member>
-    <transition name="slide" appear>
-      <board-settings
-        v-if="isOpenBoardSetting"
-        :board="board"
-        :class="{'board-settings-open': isOpenBoardSetting}"
-        @toggle-settings="toggleSettings"
-        @set-bgc="setBgc"
-        @delete-board="deleteBoard"
-        @change-date="changeDate"
-        @update-boardname="updateBoardname"
-      ></board-settings>
-    </transition>
-    <transition name="slide" appear>
-      <activitylog
-        v-if="isOpenActivitylog"
-        class="main-activitylog"
-        :isMain="true"
-        :activitieslog="board.activitieslog"
-        @close-activitylog="toggleActivitylog"
-      ></activitylog>
-    </transition>
-    <tasklist-list
-      :taskLists="board.taskLists"
-      :boardId="board._id"
-      @update-board="updateLocalBoard"
-      @update-lists="updateLists"
-      @upadte-activitylog="upadteActivitylog"
-    ></tasklist-list>
-    <router-view></router-view>
-  </section>
+    <section class="board-details" v-if="board" :style="{background: boardStyle}">
+        <div class="push"></div>
+        <board-nav
+                :boardName="board.name"
+                :members="board.members"
+                :dueDate="board.dueDate"
+                :creator="board.creator"
+                @username-changed="filterTasks"
+                @toggle-activitylog="toggleActivitylog"
+                @toggle-settings="toggleSettings"
+                @update-boardname="updateBoardname"
+                @add-member="addMember"
+        ></board-nav>
+        <add-member
+                v-if="isAddMember"
+                :users="users"
+                :members="board.members"
+                @add-new-member="addNewMember"
+                @remove-member="removeMember"
+                @close-add-member="isAddMember=false"
+        ></add-member>
+        <transition name="slide" appear>
+            <board-settings
+                    v-if="isOpenBoardSetting"
+                    :board="board"
+                    :class="{'board-settings-open': isOpenBoardSetting}"
+                    @toggle-settings="toggleSettings"
+                    @set-bgc="setBgc"
+                    @delete-board="deleteBoard"
+                    @change-date="changeDate"
+                    @update-boardname="updateBoardname"
+            ></board-settings>
+        </transition>
+        <transition name="slide" appear>
+            <activitylog
+                    v-if="isOpenActivitylog"
+                    class="main-activitylog"
+                    :isMain="true"
+                    :activitieslog="board.activitieslog"
+                    @close-activitylog="toggleActivitylog"
+            ></activitylog>
+        </transition>
+        <tasklist-list
+                :taskLists="taskLists"
+                :boardId="board._id"
+                @update-board="updateLocalBoard"
+                @update-lists="updateLists"
+                @upadte-activitylog="upadteActivitylog"
+        ></tasklist-list>
+        <router-view></router-view>
+    </section>
 </template>
 
 <script>
-import boardNav from "../components/board-nav.vue";
-import tasklistList from "../components/tasklists-list.vue";
-import activitylog from "../components/activitylog.vue";
-import boardSettings from "../components/board-settings.vue";
-import addMember from "../components/add-member.vue";
-import socketService from "../services/socket.service.js";
+    import boardNav from "../components/board-nav.vue";
+    import tasklistList from "../components/tasklists-list.vue";
+    import activitylog from "../components/activitylog.vue";
+    import boardSettings from "../components/board-settings.vue";
+    import addMember from "../components/add-member.vue";
+    import socketService from "../services/socket.service.js";
 
-export default {
-  name: "board-details",
-  data() {
-    return {
-      board: null,
-      isTaskOpen: false,
-      isDrop: true,
-      isOpenActivitylog: false,
-      isOpenBoardSetting: false,
-      isAddMember: false,
-      users: []
+    export default {
+        name: "board-details",
+        data() {
+            return {
+                board: null,
+                taskLists: [],
+                isTaskOpen: false,
+                isDrop: true,
+                isOpenActivitylog: false,
+                isOpenBoardSetting: false,
+                isAddMember: false,
+                users: [],
+            };
+        },
+        computed: {
+            boardStyle() {
+                var style;
+                if (this.board.style.color) style = this.board.style.color;
+                else if (this.board.style.url)
+                    style = `url("${this.board.style.url}") no-repeat fixed 100%`;
+                return style;
+            }
+        },
+        methods: {
+            updateLocalBoard(board) {
+                this.board = board;
+                this.taskLists = this.board.taskLists;
+            },
+            async updateBoard() {
+                const board = JSON.parse(JSON.stringify(this.board));
+                await this.$store.dispatch({
+                    type: "updateBoard",
+                    board
+                });
+                socketService.emit(
+                    "update board",
+                    JSON.parse(JSON.stringify(this.board))
+                );
+            },
+            updateLists(lists) {
+                this.board.taskLists = lists;
+                this.taskLists = this.board.taskLists;
+                this.updateBoard();
+            },
+            async filterTasks(value) {
+                let allTasks = this.board.taskLists;
+                if (value) {
+                    this.taskLists = allTasks.filter(e => e.name.startsWith(value));
+                } else {
+                    this.taskLists = allTasks;
+                }
+            },
+            async upadteActivitylog(activitylog) {
+                this.$store.commit({
+                    type: "updateActivitieslog",
+                    activitylog
+                });
+
+                this.board.activitieslog = JSON.parse(JSON.stringify(this.$store.getters.board.activitieslog));
+                this.updateBoard();
+            },
+            toggleActivitylog() {
+                this.isOpenActivitylog = !this.isOpenActivitylog;
+            },
+            toggleSettings() {
+                this.isOpenBoardSetting = !this.isOpenBoardSetting;
+            },
+            setBgc(bgc) {
+                this.board.style = bgc;
+                this.updateBoard();
+            },
+            changeDate(newDate) {
+                this.board.dueDate = newDate;
+                this.updateBoard();
+            },
+            async deleteBoard() {
+                await this.$store.dispatch({
+                    type: "deleteBoard",
+                    boardId: this.board._id
+                });
+                this.$router.push(`/`);
+            },
+            async updateBoardname(newBoardname) {
+                this.board.name = newBoardname;
+                await this.$store.dispatch({
+                    type: "updateBoard",
+                    board: this.board
+                });
+            },
+            async addMember() {
+                const users = await this.$store.dispatch({
+                    type: "getUsers"
+                });
+
+                this.isAddMember = true;
+                this.users = users;
+            },
+            async addNewMember(user) {
+                const member = {
+                    _id: user._id,
+                    username: user.username,
+                    imgUrl: user.imgUrl
+                };
+
+                this.board.members.push(member);
+
+                await this.$store.dispatch({
+                    type: "updateBoard",
+                    board: this.board
+                });
+            },
+            async removeMember(member) {
+                const memberIdx = this.board.members.findIndex(
+                    currMember => currMember._id === member._id
+                );
+
+                this.board.members.splice(memberIdx, 1);
+
+                await this.$store.dispatch({
+                    type: "updateBoard",
+                    board: this.board
+                });
+            }
+        },
+        async created() {
+            const boardId = this.$route.params.id;
+            const board = await this.$store.dispatch({
+                type: "loadBoard",
+                boardId
+            });
+            this.board = JSON.parse(JSON.stringify(board));
+            this.taskLists = this.board.taskLists;
+            socketService.setup();
+            socketService.emit("board topic", this.board._id);
+            socketService.on("update newBoard", this.updateLocalBoard);
+        },
+        destroyed() {
+            socketService.off("update newBoard", this.updateLocalBoard);
+            socketService.terminate();
+        },
+        watch: {
+            "$route.params": async function () {
+                const boardId = this.$route.params.id;
+                const board = await this.$store.dispatch({
+                    type: "loadBoard",
+                    boardId
+                });
+                this.board = JSON.parse(JSON.stringify(board));
+                socketService.emit("board topic", boardId);
+            }
+        },
+        components: {
+            boardNav,
+            tasklistList,
+            activitylog,
+            boardSettings,
+            addMember
+        }
     };
-  },
-  computed: {
-    boardStyle() {
-      var style;
-      if (this.board.style.color) style = this.board.style.color;
-      else if (this.board.style.url)
-        style = `url("${this.board.style.url}") no-repeat fixed 100%`;
-      return style;
-    }
-  },
-  methods: {
-    updateLocalBoard(board) {
-      this.board = board;
-    },
-    async updateBoard() {
-      const board = JSON.parse(JSON.stringify(this.board));
-      await this.$store.dispatch({
-        type: "updateBoard",
-        board
-      });
-      socketService.emit(
-        "update board",
-        JSON.parse(JSON.stringify(this.board))
-      );
-    },
-    updateLists(lists) {
-      this.board.taskLists = lists;
-      this.updateBoard();
-    },
-    async upadteActivitylog(activitylog) {
-      this.$store.commit({
-        type: "updateActivitieslog",
-        activitylog
-      });
-
-      this.board.activitieslog = JSON.parse(JSON.stringify(this.$store.getters.board.activitieslog));
-      this.updateBoard();
-    },
-    toggleActivitylog() {
-      this.isOpenActivitylog = !this.isOpenActivitylog;
-    },
-    toggleSettings() {
-      this.isOpenBoardSetting = !this.isOpenBoardSetting;
-    },
-    setBgc(bgc) {
-      this.board.style = bgc;
-      this.updateBoard();
-    },
-    changeDate(newDate) {
-      this.board.dueDate = newDate;
-      this.updateBoard();
-    },
-    async deleteBoard() {
-      await this.$store.dispatch({
-        type: "deleteBoard",
-        boardId: this.board._id
-      });
-      this.$router.push(`/`);
-    },
-    async updateBoardname(newBoardname) {
-      this.board.name = newBoardname;
-      await this.$store.dispatch({
-        type: "updateBoard",
-        board: this.board
-      });
-    },
-    async addMember() {
-      const users = await this.$store.dispatch({
-        type: "getUsers"
-      });
-
-      this.isAddMember = true;
-      this.users = users;
-    },
-    async addNewMember(user) {
-      const member = {
-        _id: user._id,
-        username: user.username,
-        imgUrl: user.imgUrl
-      };
-
-      this.board.members.push(member);
-
-      await this.$store.dispatch({
-        type: "updateBoard",
-        board: this.board
-      });
-    },
-    async removeMember(member) {
-      const memberIdx = this.board.members.findIndex(
-        currMember => currMember._id === member._id
-      );
-
-      this.board.members.splice(memberIdx, 1);
-
-      await this.$store.dispatch({
-        type: "updateBoard",
-        board: this.board
-      });
-    }
-  },
-  async created() {
-    const boardId = this.$route.params.id;
-    const board = await this.$store.dispatch({
-      type: "loadBoard",
-      boardId
-    });
-    this.board = JSON.parse(JSON.stringify(board));
-    socketService.setup();
-    socketService.emit("board topic", this.board._id);
-    socketService.on("update newBoard", this.updateLocalBoard);
-  },
-  destroyed() {
-    socketService.off("update newBoard", this.updateLocalBoard);
-    socketService.terminate();
-  },
-  watch: {
-    "$route.params": async function() {
-      const boardId = this.$route.params.id;
-      const board = await this.$store.dispatch({
-        type: "loadBoard",
-        boardId
-      });
-      this.board = JSON.parse(JSON.stringify(board));
-      socketService.emit("board topic", boardId);
-    }
-  },
-  components: {
-    boardNav,
-    tasklistList,
-    activitylog,
-    boardSettings,
-    addMember
-  }
-};
 </script>
